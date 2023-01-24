d6c0a922e9a0bec9ff2aa1cb146dfae7673b3696
Pader Xiong <paderx26@gmail.com>
2023-01-23 22:01:32 -0600

    added favoriteScreen

---
+++ screens/FavoriteScreen.js
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux";
+import { View, FlatList, Text } from "react-redux";
+import { Avatar } from "react-native-elements";
+import Loading from "../components/LoadingComponent";
+import { baseURL } from "../shared/baseUrl";
+
+const FavortiesScreen = { navigation };
+
+export default FavortiesScreen;
